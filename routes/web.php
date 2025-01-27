d8e85acfacae0e61c2c887e5e78d3a7251ac03b2
Sahiru Lakshan <86883938+SahiruLakshan@users.noreply.github.com>
2025-01-27 22:30:08 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
+
+use App\Http\Controllers\BudgetController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\SubCategoryController;
-
 use App\Http\Controllers\AssetsController;
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,18 +21,17 @@ Route::get('/', function () {
     return view('dashboard.dashboard');
 });
 
-Route::get('/budgetmaster', function () {
-    return view('dashboard.budgetmaster');
-});
+//Budget Process
+Route::get('/budgetmaster', [BudgetController::class, 'master'])->name('budget.master');
+Route::post('/submit-budget', [BudgetController::class, 'store'])->name('budget.store');
+Route::get('/budget', [BudgetController::class, 'index'])->name('budget.index');
+Route::get('/budgetprocess', [BudgetController::class, 'sub'])->name('budget.sub');
+Route::post('/budgetplansubmit', [BudgetController::class, 'budgetplansubmit'])->name('budgetplan.store');
 
-Route::get('/subpage', function () {
-    return view('dashboard.budgetsub');
-});
 
 Route::get('/compage', function () {
     return view('dashboard.budgetview');
 });
-
 
 Route::get('/category', function () {
     return view('dashboard.asset_manage.add_catogery');
@@ -49,16 +48,12 @@ Route::get('/subcategory', function () {
     return view('dashboard.asset_manage.add_sub_catogery');
 });
 
-
 Route::get('/sub_category', [SubCategoryController::class, 'index']);
 Route::get('/subcategoryview', [SubCategoryController::class, 'show'])->name('subcategories.show');
 Route::get('/editsubcategories/{id}', [SubCategoryController::class, 'edit'])->name('subcategories.edit');
 Route::put('/subcategories/{id}', [SubCategoryController::class, 'update'])->name('subcategories.update');
 Route::post('/create_sub_category', [SubCategoryController::class, 'store'])->name('subcategories.store');
 Route::delete('/deletesubcategories/{id}', [SubCategoryController::class, 'destroy'])->name('subcategories.destroy');
-
-
-
 
 //Add new asset
 Route::get('/add_asserts', function () {
@@ -73,9 +68,7 @@ Route::put('/assets/update/{id}', [AssetsController::class, 'update'])->name('as
 Route::delete('/assets/destroy/{id}', [AssetsController::class, 'destroy'])->name('assets.destroy');
 Route::get('/get-subcategories', [AssetsController::class, 'getSubcategories'])->name('get.subcategories');
 
-
 //allocate_asserts
 Route::get('/allocate_asserts', function () {
     return view('dashboard.asset_manage.allocate_asserts');
 });
-
